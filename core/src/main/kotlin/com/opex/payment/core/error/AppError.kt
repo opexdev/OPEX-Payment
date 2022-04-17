5117655ecab9164350442b6fd910cca384f67dd9
Peyman <marchosiax@gmail.com>
2022-04-17 17:20:40 +0430

    Fix schedule service

--- core/src/main/kotlin/com/opex/payment/core/error/AppError.kt
+++ core/src/main/kotlin/com/opex/payment/core/error/AppError.kt
@@ -15,7 +15,7 @@ enum class AppError(val code: Int, val message: String?, val status: HttpStatus)
     AlreadyVerified(13000, "This payment is already verified", HttpStatus.BAD_REQUEST),
     VerificationFailed(13001, "Unable to verify", HttpStatus.INTERNAL_SERVER_ERROR),
     VerificationNotAllowed(13002, "Verification is not allowed for this payment", HttpStatus.FORBIDDEN),
-    PaymentNotAllowed(13003, "Payment is not allowed for this payment", HttpStatus.FORBIDDEN),
+    PaymentNotAllowed(13003, "Payment is not allowed for this invoice", HttpStatus.FORBIDDEN),
     OpenPayments(13004, "Found open payments for user. Finish or cancel open payments", HttpStatus.BAD_REQUEST),
     PaymentLocked(13005, "Payment is locked for this invoice. Please try again later", HttpStatus.BAD_REQUEST),
     InvalidPaymentParams(13006, "Payment parameters are invalid", HttpStatus.BAD_REQUEST),
--- core/src/main/kotlin/com/opex/payment/core/error/AppException.kt
+++ core/src/main/kotlin/com/opex/payment/core/error/AppException.kt
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus
 class AppException(
     val error: AppError,
     message: String? = null,
-    val status: HttpStatus? = null,
+    val status: HttpStatus = error.status,
     val data: Any? = null,
     val crimeScene: Class<*>? = null
 ) : RuntimeException(message ?: error.message)
